598abd262822844e6565f1953533bec6c74147bc
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2024-07-08 21:12:59 +0530

    accessing req.headers

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const port = 3000;
 
 const app = express();
 
+app.post("/conversations", (req, res) => {
+  console.log(req.headers["authorization"]);
+  res.send("<b>2+2=4<b>");
+});
+
 app.get("/", (req, res) => {
+  console.log(req.headers["authorization"]);
   res.send("<b>Hello World!<b>");
 });
 
